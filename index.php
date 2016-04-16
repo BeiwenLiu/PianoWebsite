6ee254d4993ea6e09c8cf8f390f58f23d83478f2
Beiwen Liu <beiwenl@gmail.com>
2016-04-16 03:21:42 -0400

    Change styling

--- index.php
+++ index.php
@@ -6,6 +6,9 @@
 
 	<!-- Set the viewport width to device width for mobile -->
   	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+  	<script src="./js/jquery-1.10.0.min.js"></script>
+	<script src="./js/bootstrap/js/bootstrap.min.js"></script>
+	<script src="./js/script.js"></script>
 
 	<!-- Included Bootstrap CSS Files -->
 	<link rel="stylesheet" href="./js/bootstrap/css/bootstrap.min.css" />
@@ -14,19 +17,21 @@
 	<!-- Css -->	
 	<link rel="stylesheet" href="./css/style.css" />
 
+	<script src="/js/navFloat.js"></script>
+
 </head>
 <body id="start">
 
 
 	<div id="navigation"> 
-		
+
 
 	</div>
 	<!-- This is the overlay DIV that is displayed while loading a new page -->
 	<div id="loader"></div>
 
 	<div id="container1"> 
-
+			<span id="text"> Beiwen Liu </span>
         </div>
 
 	<div class="container">
@@ -36,19 +41,21 @@
 			<div class="content">
 				<div class="span12">
 					<div class="item">
-						<div class="img"><iframe
+						<div class="img">
+							<iframe
 						src="http://www.youtube.com/embed/Hz9C-5Mnes0" frameborder="0"  id="testing" allowfullscreen>
 						</iframe>
 					</div>
 						<div class="caption">
-							<h4>Lorem Ipsum 1</h4>
-							<p>dolore sit amen</p>
+							<h4>Last updated: 4/16/2016</h4>
 						</div>
 					</div>	
 				</div>
-				<div class="span3">
+				<div class="span12">
 					<div class="item">
-						<div class="img"></div>
+						<div class="img"><iframe
+						src="http://www.youtube.com/embed/o2GDKg7HiWg" frameborder="0"  id="testing" allowfullscreen>
+						</iframe></div>
 						<div class="caption">
 							<h4>Lorem Ipsum 2</h4>
 							<p>dolore sit amen</p>
@@ -158,10 +165,5 @@
 		<a href="./page5.php"></a>
 		<a href="./page6.php"></a>
 	</div>
-	
-
-	<script src="./js/jquery-1.10.0.min.js"></script>
-	<script src="./js/bootstrap/js/bootstrap.min.js"></script>
-	<script src="./js/script.js"></script>
 </body>
 </html>
---
+++ js/navFloat.js
@@ -0,0 +1,20 @@
+$(document).ready(function () {  
+  var top = $('#navigation').offset().top - parseFloat($('#navigation').css('marginTop').replace(/auto/, 100));
+  $(window).scroll(function (event) {
+    // what the y position of the scroll is
+    var y = $(this).scrollTop();
+
+    // whether that's below the form
+    if (y >= top) {
+      // if so, ad the fixed class
+      $('#navigation').addClass('fixed');
+    } else {
+      // otherwise remove it
+      $('#navigation').removeClass('fixed');
+    }
+  });
+
+  $("#text").hide(0).fadeIn(1000);
+});
+
+
