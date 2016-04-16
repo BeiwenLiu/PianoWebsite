ceaa1935459ab0ed5873a6ec57eb52f053e7875b
Beiwen Liu <beiwenl@gmail.com>
2016-04-16 02:17:11 -0400

    Second commit

---
+++ index.php
@@ -0,0 +1,167 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />	
+	<title>Beiwen Liu</title>
+
+	<!-- Set the viewport width to device width for mobile -->
+  	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+	<!-- Included Bootstrap CSS Files -->
+	<link rel="stylesheet" href="./js/bootstrap/css/bootstrap.min.css" />
+	<link rel="stylesheet" href="./js/bootstrap/css/bootstrap-responsive.min.css" />
+	
+	<!-- Css -->	
+	<link rel="stylesheet" href="./css/style.css" />
+
+</head>
+<body id="start">
+
+
+	<div id="navigation"> 
+		
+
+	</div>
+	<!-- This is the overlay DIV that is displayed while loading a new page -->
+	<div id="loader"></div>
+
+	<div id="container1"> 
+
+        </div>
+
+	<div class="container">
+
+		<!-- This is the main container (#scroll-container) -->
+		<div id="scroll-container" class="row">
+			<div class="content">
+				<div class="span12">
+					<div class="item">
+						<div class="img"><iframe
+						src="http://www.youtube.com/embed/Hz9C-5Mnes0" frameborder="0"  id="testing" allowfullscreen>
+						</iframe>
+					</div>
+						<div class="caption">
+							<h4>Lorem Ipsum 1</h4>
+							<p>dolore sit amen</p>
+						</div>
+					</div>	
+				</div>
+				<div class="span3">
+					<div class="item">
+						<div class="img"></div>
+						<div class="caption">
+							<h4>Lorem Ipsum 2</h4>
+							<p>dolore sit amen</p>
+						</div>
+					</div>	
+				</div>
+				<div class="span3">
+					<div class="item">
+						<div class="img"></div>
+						<div class="caption">
+							<h4>Lorem Ipsum 3</h4>
+							<p>dolore sit amen</p>
+						</div>
+					</div>	
+				</div>
+				<div class="span3">
+					<div class="item">
+						<div class="img"></div>
+						<div class="caption">
+							<h4>Lorem Ipsum 4</h4>
+							<p>dolore sit amen</p>
+						</div>
+					</div>	
+				</div>
+				<div class="span3">
+					<div class="item">
+						<div class="img"></div>
+						<div class="caption">
+							<h4>Lorem Ipsum 5</h4>
+							<p>dolore sit amen</p>
+						</div>
+					</div>	
+				</div>
+				<div class="span3">
+					<div class="item">
+						<div class="img"></div>
+						<div class="caption">
+							<h4>Lorem Ipsum 6</h4>
+							<p>dolore sit amen</p>
+						</div>
+					</div>	
+				</div>
+				<div class="span3">
+					<div class="item">
+						<div class="img"></div>
+						<div class="caption">
+							<h4>Lorem Ipsum 7</h4>
+							<p>dolore sit amen</p>
+						</div>
+					</div>	
+				</div>
+				<div class="span3">
+					<div class="item">
+						<div class="img"></div>
+						<div class="caption">
+							<h4>Lorem Ipsum 8</h4>
+							<p>dolore sit amen</p>
+						</div>
+					</div>	
+				</div>
+				<div class="span3">
+					<div class="item">
+						<div class="img"></div>
+						<div class="caption">
+							<h4>Lorem Ipsum 9</h4>
+							<p>dolore sit amen</p>
+						</div>
+					</div>	
+				</div>
+				<div class="span3">
+					<div class="item">
+						<div class="img"></div>
+						<div class="caption">
+							<h4>Lorem Ipsum 10</h4>
+							<p>dolore sit amen</p>
+						</div>
+					</div>	
+				</div>
+				<div class="span3">
+					<div class="item">
+						<div class="img"></div>
+						<div class="caption">
+							<h4>Lorem Ipsum 11</h4>
+							<p>dolore sit amen</p>
+						</div>
+					</div>	
+				</div>
+				<div class="span3">
+					<div class="item">
+						<div class="img"></div>
+						<div class="caption">
+							<h4>Lorem Ipsum 12</h4>
+							<p>dolore sit amen</p>
+						</div>
+					</div>	
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<!-- These are all the links that can be loaded. -->
+	<div id="pages">
+		<a href="./index.php"></a>
+		<a href="./page2.php"></a>
+		<a href="./page3.php"></a>
+		<a href="./page4.php"></a>
+		<a href="./page5.php"></a>
+		<a href="./page6.php"></a>
+	</div>
+	
+
+	<script src="./js/jquery-1.10.0.min.js"></script>
+	<script src="./js/bootstrap/js/bootstrap.min.js"></script>
+	<script src="./js/script.js"></script>
+</body>
+</html>
